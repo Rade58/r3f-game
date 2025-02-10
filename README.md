2ca644a652e9d82689823afbc49910147ab965a6
rade <bajic.rade2@gmail.com>
2025-02-10 12:45:01 +0100

    initial

---
+++ README.md
@@ -0,0 +1,29 @@
+# R3F Workshops - Game
+
+- Bootstraped with:
+
+```
+pnpm create vite
+```
+
+- dependancies
+
+```
+pnpm add three @react-three/fiber @react-three/drei leva@0.9.34
+```
+
+```
+pnpm add -D r3f-perf @types/three
+```
+
+# `@react-three/rapier`
+
+```
+pnpm add @react-three/rapier
+```
+
+# Leva @0.9.34
+
+---
+
+---
---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+// -----------------------------------------------------------
+// import { App } from "./0_setup/App"; // with Stage, no light, no shadows enabled
+// import { App } from "./1_setup/App"; // with lights without Stage
+// import { App } from "./3_setup/App";
+// -----------------------------------------------------------
+import { App } from "./4_setup/App"; // floor is a box, not a plane
+// -----------------------------------------------------------
+
+// -----------------------------------------------------------
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
